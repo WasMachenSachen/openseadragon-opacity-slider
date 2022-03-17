c64414441bff7b6b6481cc14f6fbbd43fb72405c
WasMachenSachen <loges.info@googlemail.com>
2022-03-17 10:08:39 +0100

    feat: demo with options

--- index.js
+++ index.js
@@ -1,51 +1,82 @@
 import "./src/plugin.js";
 
 let sources = ["images/example-image.png", "images/example-image-bw.png"];
+let viewer;
 
-const viewer = OpenSeadragon({
-  id: "openseadragon1",
-  showNavigationControl: false,
-});
+function initOsdOsPlugin(newOptions) {
+  /*
+   * Init OSD
+   */
+  viewer = OpenSeadragon({
+    id: "openseadragon1",
+    showNavigationControl: false,
+  });
 
-// viewer.addTiledImage({
-//   tileSource: sources[0],
-//   opacity: 0.5,
-// });
+  /*
+   * Add Tile Image example
+   */
+  // viewer.addTiledImage({
+  //   tileSource: sources[0],
+  //   opacity: 0.5,
+  // });
 
-viewer.addSimpleImage({
-  url: sources[1],
-});
-viewer.addSimpleImage({
-  url: sources[0],
-});
+  /*
+   * Add Simple Image example
+   */
+  viewer.addSimpleImage({
+    url: sources[1],
+  });
+  viewer.addSimpleImage({
+    url: sources[0],
+  });
 
-const layerInformation = [
-  {
-    layerName: "Example",
-    layerNameExplanation: "This is a flower pot",
-    source: sources[0],
-    hidden: false,
-  },
-  {
-    layerName: "Black and White",
-    layerNameExplanation: "This is a flower pot in Black&White",
-    source: sources[1],
-    hidden: false,
-  },
-];
-const options = {
-  sortable: true,
-  showRangeSlider: true,
-  showLayerPicker: true,
-  showNumberInput: true,
-  showRemove: true,
-  showtempLayerInformation: true,
-  showSliders: true,
-  layerPickerHeading: "Choose Layer",
-  layerPickerButtonText: "Layer Picker",
-};
+  /*
+   * Some basic layer Information
+   */
+  const layerInformation = [
+    {
+      layerName: "Example",
+      layerNameExplanation: "This is a flower pot",
+      source: sources[0],
+      hidden: false,
+    },
+    {
+      layerName: "Black and White",
+      layerNameExplanation: "This is a flower pot in Black&White",
+      source: sources[1],
+      hidden: false,
+    },
+  ];
+  /*
+   * Enable overwritting options via function parameter to change them dynamic in demo
+   */
+  const options = newOptions
+    ? newOptions
+    : {
+        sortable: true,
+        showRangeSlider: true,
+        showLayerPicker: true,
+        showNumberInput: true,
+        showRemove: true,
+        showLayerInformation: true,
+        layerPickerHeading: "Choose Layer",
+        layerPickerButtonText: "Layer Picker",
+      };
+  console.log(options);
+  /*
+   * init opacity slider plugin
+   */
+  viewer.opacityslider({
+    ...options,
+    layerInformation,
+  });
+}
+initOsdOsPlugin();
 
-viewer.opacityslider({
-  ...options,
-  layerInformation,
-});
+function destroyOsd() {
+  viewer.destroy();
+  viewer = null;
+  document.getElementById("opacitySliderWrapper").innerHTML = "";
+}
+window.destroyOsd = destroyOsd;
+window.initOsdOsPlugin = initOsdOsPlugin;
